eab753dc9953c433e19f72d57465770b7bb02665
Abhi <makker.abhinav@gmail.com>
2024-12-21 14:52:13 +0530

    Node scheduler

---
+++ index.js
@@ -0,0 +1,11 @@
+import { scheduleJob } from "node-schedule";
+
+//Runs on every 21st of the month on 14:47 PM
+// scheduleJob("47 14 21 * *", () => {
+//   console.log("Running a job");
+// });
+
+// Runs every second (6 stars)
+// scheduleJob("*/1 * * * * *", () => {
+//   console.log("Running a job");
+// });
